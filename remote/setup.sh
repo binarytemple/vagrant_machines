5b3271e9ed10b85e5cda20e931f23e17ffda32d8
bryan hunt <bryan.hunt@picsolve.com>
2014-01-02 16:55:24 +0000

    working config, requires user to run setup.sh on first login

---
+++ remote/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git clone git@bitbucket.org:bryan_picsolve/poc_docker.git
+
+git clone git@github.com:Picsolve/bryan_scratch.git
+
+git clone git@github.com/Picsolve/media-management-system
