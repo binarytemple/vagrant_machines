3ef452dda09e35689cc5c76a3b1cf97cb89cd37c
bryan hunt <github@binarytemple.co.uk>
2014-04-17 14:59:24 +0100

    latest

--- ec2/rolling_restart.zsh
+++ ec2/rolling_restart.zsh
@@ -4,4 +4,9 @@ eval `sed -n  '/aws_secret_access_key/{s_[^=]*=__;s_ *__;s/.*/export AWS_SECRET_
 eval `sed -n '/aws_access_key_id/{s_[^=]*=__;s_ *__;s/.*/export AWS_ACCESS_KEY=&/;p;}' < ~/.boto`
 export ANSIBLE_HOST_KEY_CHECKING=False
 
-/common/ansible/bin/ansible-playbook -vvvv -i local boot.yml
+if [[ ( ! -e ./roles ) || ( ! -e ./roles/basho.riak ) || ( ! -e  ./roles/basho.riak-common )  ]] 
+then 
+	ansible-galaxy install -r roles.txt -p roles
+fi
+
+/common/ansible/bin/ansible-playbook -vvvv -i ec2.py boot.yml
