1d0932f6d212c070927d94fc1a8cf9d041ad708e
bryan hunt <github@binarytemple.co.uk>
2014-05-08 16:22:54 +0100

    shedload of improvements

--- ec2/boot_spot.zsh
+++ ec2/boot_spot.zsh
@@ -1,12 +1,4 @@
 #!/bin/zsh
 
-eval `sed -n  '/aws_secret_access_key/{s_[^=]*=__;s_ *__;s/.*/export AWS_SECRET_KEY=&/;p;}' < ~/.boto`
-eval `sed -n '/aws_access_key_id/{s_[^=]*=__;s_ *__;s/.*/export AWS_ACCESS_KEY=&/;p;}' < ~/.boto`
-export ANSIBLE_HOST_KEY_CHECKING=False
-
-if [[ ( ! -e ./roles ) || ( ! -e ./roles/basho.riak ) || ( ! -e  ./roles/basho.riak-common )  ]] 
-then 
-	ansible-galaxy install -r roles.txt -p roles
-fi
-
-/Common/ansible/bin/ansible-playbook -vvvv -i ec2.py -u ubuntu boot_spot.yml
+source ./common.zsh
+ansible-playbook -vvvv -i ec2.py -u ubuntu boot_spot.yml
