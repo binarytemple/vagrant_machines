f35de9398460f5ca6977ede10a6f94daf2c6eb68
Bryan Hunt <github@binarytemple.co.uk>
2015-12-18 16:39:54 +0000

    Delete setup.sh

--- remote/default_user/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git clone git@bitbucket.org:bryan_picsolve/poc_docker.git
-
-git clone git@github.com:Picsolve/bryan_scratch.git
-
-git clone git@github.com/Picsolve/media-management-system
